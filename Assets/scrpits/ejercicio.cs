cee595c5afeb82fdbcc6b5f4161fc6f93a5bfb5e
CarolaOrbuch <101721194+CarolaOrbuch@users.noreply.github.com>
2022-04-18 13:55:50 -0300

    added details
    detalles cambiados en busca de errores.

--- Assets/scrpits/ejercicio.cs
+++ Assets/scrpits/ejercicio.cs
@@ -1,7 +1,6 @@
 ﻿//Algoritmo al que se ingresa por inspector edad en años y su altura en metros.
 //Si la altura ingresada es mayor a 1,2 metros o si la edad es mayor a 13 permitir la entrada, 
 //caso contrario denegar el acceso.
-
 //Debe devolver mensajes de error si la edad es menor que 1 o si la altura es negativa.
 using System.Collections;
 using System.Collections.Generic;
@@ -14,11 +13,11 @@ public class ejercicio : MonoBehaviour
 
     void Start()
     {
-        if (altura > 1.2 && edad >= 13)
+        if (altura >= 1.2 && edad >= 13)
         {
             Debug.Log("bienvenido");
         }
-        else if (edad<1 || altura < 0)
+        else if (edad <= 1 || altura <= 0)
         {
             Debug.Log("acceso denegado");
         }
